c1c4be8464bf9a33e56adcafcaf87efa714fb5f1
Abhishek12589 <hindujaabhishek5@gmail.com>
2024-08-03 12:38:41 +0530

    Working on Redux Toolkit

--- src/Components/Instamart.js
+++ src/Components/Instamart.js
@@ -1,8 +1,36 @@
+import { useState } from "react"
+
+const Section = ({title,description})=>{
+    const [isvisible, setisvisible] = useState((false))
+    return(
+        <div className="border border-black m-1">
+            <h3 className="font-bold text-xl p-1">{title}</h3>
+            {isvisible ? (
+                    <button className = "p-1" onClick={()=> setisvisible(false)}>hide</button>
+                ):
+                <button className="p-1" onClick={()=> setisvisible(true)}>show</button>
+            }
+            {isvisible && <p className="p-1">{description}</p>}
+        </div>
+    )
+}
+
 const Instamart = ()=>{
     return (
         <div>
-            <h1>100s of Component Inside</h1>
-            <h2>Component Inside Component</h2>
+        <h1 className="text-3xl p-2 m-2">Instamart</h1>
+        <Section
+            title={"About Instamart"}
+            description={"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui dicta minus molestiae vel beatae natus eveniet ratione temporibus aperiam harum alias officiis assumenda officia quibusdam deleniti eos cupiditate dolore doloribus!"}
+        />
+        <Section
+            title={"Team Instamart"}
+            description={"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui dicta minus molestiae vel beatae natus eveniet ratione temporibus aperiam harum alias officiis assumenda officia quibusdam deleniti eos cupiditate dolore doloribus!"}
+        />
+        <Section
+            title={"Career Instamart"}
+            description={"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui dicta minus molestiae vel beatae natus eveniet ratione temporibus aperiam harum alias officiis assumenda officia quibusdam deleniti eos cupiditate dolore doloribus!"}
+        />
         </div>
     )
 }
